fa09e7ff964a54c253db28236b93c3f009a70a39
lizhihong <zhihong.li@petkit.com>
2022-08-02 11:51:42 +0800

    build(script): remove chalk and cross-spawn

--- scripts/bin/chu-scripts.js
+++ scripts/bin/chu-scripts.js
@@ -3,7 +3,8 @@
 const { join } = require('path');
 const assert = require('assert');
 const { existsSync } = require('fs');
-const { sync } = require('@chu/utils/compiled/cross-spawn');
+// const { sync } = require('@chu/utils/compiled/cross-spawn');
+const { sync } = require('../compiled/cross-spawn');
 // const { sync } = require('cross-spawn');
 const chalk = require('../compiled/chalk').default;
 // const chalk = require('@chu/utils/compiled/chalk').default;
